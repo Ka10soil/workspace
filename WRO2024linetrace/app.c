df076927cb7096f8dfff4803e68652a0a9401911
Na0toWatabe <82302069+NaotoWatase@users.noreply.github.com>
2023-12-29 11:10:01 +0900

    linetraceは保留。rgbのテストプログラム作成

--- WRO2024linetrace/app.c
+++ WRO2024linetrace/app.c
@@ -22,6 +22,10 @@
 #define _debug(x)
 #endif
 
+static FILE *bt = NULL;
+
+FILE *file;
+const char* logfilename = "/wro_result.txt";
 /**
  * Define the connection ports of the sensors and motors.
  * By default, this application uses the following ports:
@@ -30,6 +34,8 @@
  * Left motor:   Port B
  * Right motor:  Port C
  */
+int battery;
+
 static const sensor_port_t  PortSensorColor1 = EV3_PORT_1;
 static const sensor_port_t  PortSensorColor2 = EV3_PORT_2; //左
 static const sensor_port_t  PortSensorColor3 = EV3_PORT_3; //右
@@ -43,6 +49,7 @@ static const motor_port_t   PortMotorArmDown = EV3_PORT_D; //オブジェを落�
 
 void stopping();
 void gain_set(int power, float *p_gain, float *d_gain);
+void gain_set_pid(int power, float *p_gain, float *d_gain, float *i_gain);
 void linetrace_cm_pd_SP(float cm, int power, bool_t stop);
 
 static void button_clicked_handler(intptr_t button) {
@@ -101,6 +108,34 @@ void gain_set(int power, float *p_gain, float *d_gain){
     }
 }
 
+void gain_set_pid(int power, float *p_gain, float *d_gain, float *i_gain){
+    *p_gain = 1.4;
+    *d_gain = 100;
+    *i_gain = 0.00001;
+    if(power > 40){
+        *p_gain = 1.2;   
+        *d_gain = 200;
+        *i_gain = 0.000000;
+    }
+    if(power > 50){
+        *p_gain = 2;   
+        *d_gain = 1500;
+        *i_gain = 0.000000;
+    }
+    if(power > 60){
+        *p_gain = 1;   
+        *d_gain = 600;
+        *i_gain = 0.00000;
+    }
+    if(power > 65){
+        *p_gain = 1;   
+        *d_gain = 300;
+        *i_gain = 0.00000;
+    }
+    
+    
+    
+}
 
 
 void linetrace_cm_pd_SP(float cm, int power, bool_t stop){
@@ -134,7 +169,7 @@ void linetrace_cm_pd_SP(float cm, int power, bool_t stop){
         last_diff = diff;
         diff = now_reflect_2 - now_reflect_3;
         d = (diff - last_diff);
-        steering = diff * p_gain + d * d_gain;
+        steering = diff * 0.3 + d * 100;
         if(steering > 0) {
             lb_power = power;
             rc_power = power - (power * steering / 50);
@@ -154,10 +189,138 @@ void linetrace_cm_pd_SP(float cm, int power, bool_t stop){
     ev3_motor_stop(EV3_PORT_C, stop);
 }
 
+void linetrace_new(float cm, int power, bool_t stop){
+    ev3_motor_reset_counts(EV3_PORT_B);
+    ev3_motor_reset_counts(EV3_PORT_C);
+    int now_angle_lb = 0;
+    int now_angle_rc = 0;
+    int average = 0;
+    int lb_power;
+    int rc_power;
+    int now_reflect_2; 
+    int now_reflect_3; 
+    int last_diff = 0;
+    int diff = 0;
+    float d;
+    float i = 0;
+    int steering;
+    float p_gain;
+    float d_gain;
+    float i_gain;
+    float max_power = 40;
+    gain_set(power, &p_gain, &d_gain);
+
+    now_reflect_2 = ev3_color_sensor_get_reflect(EV3_PORT_2);
+    now_reflect_3 = ev3_color_sensor_get_reflect(EV3_PORT_3);
+    diff = now_reflect_2 - now_reflect_3;
+    while (true) {
+        
+        now_reflect_2 = ev3_color_sensor_get_reflect(EV3_PORT_2);
+        now_reflect_3 = ev3_color_sensor_get_reflect(EV3_PORT_3);
+        now_angle_lb = abs(ev3_motor_get_counts(EV3_PORT_B));
+        now_angle_rc = abs(ev3_motor_get_counts(EV3_PORT_C));
+        average = (now_angle_lb + now_angle_rc) / 2;
+        power = (max_power / (20*ROBOT1CM) / (20*ROBOT1CM)) * average * average + 30;
+        if (power > 70) power = 70;
+        gain_set_pid(power, &p_gain, &d_gain, &i_gain);
+        last_diff = diff;
+        diff = now_reflect_2 - now_reflect_3;
+        i = diff + i;
+        d = (diff - last_diff);
+        steering = diff * p_gain + d * d_gain + i * i_gain;
+        if(steering > 0) {
+            lb_power = power;
+            rc_power = power - (power * steering / 50);
+            lb_power = -lb_power;
+        }
+        else {
+            lb_power = power + (power * steering / 50);
+            rc_power = power;
+            lb_power = -lb_power;
+        }
+        ev3_motor_set_power(EV3_PORT_B, lb_power);
+        ev3_motor_set_power(EV3_PORT_C, rc_power);
+
+        if (average >= ROBOT1CM*cm) break;
+    }
+    ev3_motor_stop(EV3_PORT_B, stop);
+    ev3_motor_stop(EV3_PORT_C, stop);
+}
+
+void linetrace_rgb(float cm, int power, bool_t stop){
+    rgb_raw_t rgb_val;//カラーセンサーの値を保存するために必要な変数(必須)
+    float red2 = 0;
+    float green2 = 0;
+    float blue2 = 0;
+    float red3 = 0;
+    float green3 = 0;
+    float blue3 = 0;
+    float judgement = 0;
+    int lb_power;
+    int rc_power;
+    int steering;
+    int last_diff = 0;
+    int diff = 0;
+    float d;
+    ev3_motor_reset_counts(EV3_PORT_B);
+    ev3_motor_reset_counts(EV3_PORT_C);
+    int now_angle_lb = 0;
+    int now_angle_rc = 0;
+    int average = 0;
+    ev3_color_sensor_get_rgb_raw(EV3_PORT_2, &rgb_val);
+    red2 = rgb_val.r;
+    green2 = rgb_val.g;
+    blue2 = rgb_val.b;
+    ev3_color_sensor_get_rgb_raw(EV3_PORT_3, &rgb_val);
+    red3 = rgb_val.r;
+    green3 = rgb_val.g;
+    blue3 = rgb_val.b;
+    judgement = (red2 + green2 + blue2) - (red3 + green3 + blue3);
+    diff = judgement;
+    while (true){
+        now_angle_lb = abs(ev3_motor_get_counts(EV3_PORT_B));
+        now_angle_rc = abs(ev3_motor_get_counts(EV3_PORT_C));
+        average = (now_angle_lb + now_angle_rc) / 2;
+        ev3_color_sensor_get_rgb_raw(EV3_PORT_2, &rgb_val);
+        red2 = rgb_val.r;
+        green2 = rgb_val.g;
+        blue2 = rgb_val.b;
+        ev3_color_sensor_get_rgb_raw(EV3_PORT_3, &rgb_val);
+        red3 = rgb_val.r;
+        green3 = rgb_val.g;
+        blue3 = rgb_val.b;
+        judgement = ((red2 + green2 + blue2) - (red3 + green3 + blue3)) / 7.5;
+
+        last_diff = diff;
+        diff = judgement;
+        d = (diff - last_diff);
+        steering = judgement * 0.4 + d * 80;
+        //steering = judgement * 0.4 + d * 80  po=18;
+        
+        if(steering > 0) {
+            lb_power = power;
+            rc_power = power - (power * steering / 50);
+            lb_power = -lb_power;
+        }
+        else {
+            lb_power = power + (power * steering / 50);
+            rc_power = power;
+            lb_power = -lb_power;
+        }
+        ev3_motor_set_power(EV3_PORT_B, lb_power);
+        ev3_motor_set_power(EV3_PORT_C, rc_power);
+        /*fprintf(bt,"2_RGB:%f,%f,%f 3_RGB:%f,%f,%f JUDGE:%f\r\n", red2, green2, blue2, red3, green3, blue3, judgement);*/
+        if (average >= ROBOT1CM*cm) break;
+    }
+    ev3_motor_stop(EV3_PORT_B, stop);
+    ev3_motor_stop(EV3_PORT_C, stop);
+
+}
 
 void main_task(intptr_t unused) {
 
-
+    bt = ev3_serial_open_file(EV3_SERIAL_BT);
+    assert(bt != NULL);
 
     /* Register button handlers */
     ev3_button_set_on_clicked(BACK_BUTTON, &button_clicked_handler, BACK_BUTTON);
@@ -173,16 +336,30 @@ void main_task(intptr_t unused) {
     ev3_sensor_config(PortSensorColor2, COLOR_SENSOR);
     ev3_sensor_config(PortSensorColor3, COLOR_SENSOR);
     ev3_sensor_config(PortSensorColor4, COLOR_SENSOR);
+    /*
+    fprintf(bt, "----GAME_START----\r\n");
+    file=fopen(logfilename,"a");//ファイルをオープン(名前の指定)
+    fprintf(file, "----GAME_START----\r\n");
+    fclose(file);
 
-
+    battery = ev3_battery_voltage_mV();
+    fprintf(bt, "BATTERY:%d\r\n", battery);
+    file=fopen(logfilename,"a");//ファイルをオープン(名前の指定)
+    fprintf(file, "BATTERY:%d\r\n", battery);
+    fclose(file);
+    */
     
 
     /* ここからコーディング */
     stopping();
-    linetrace_cm_pd_SP(100,70,true);
-    stopping();
-    linetrace_cm_pd_SP(100,70,true);
-    stopping();
+    while (true)
+    {
+        linetrace_rgb(100,20,true);
+        stopping();
+    }
+    
+    
+
 
 
 }
